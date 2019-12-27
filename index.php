99a815c64ca21a5688262865d5b0f4b67de61c19
joey <joey.jreij@zolemba.com>
2019-12-27 08:22:21 +0100

    moved inline styling to assets file

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 
-  <title>Uptime Monitor</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+    <title>Uptime Monitor</title>
+    <meta name="description" content="Uptime monitor">
+    <meta name="author" content="MarkDijkstra">
 
-<!--  <link rel="stylesheet" href="css/styles.css?v=1.0">-->
+    <link rel="stylesheet" href="assets/css/core.css?v=1.0">
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
@@ -38,67 +38,6 @@
             stamp;
         });
     </script>
-
-    <style>
-        .pingblock{
-            min-width: 300px;
-            margin:0 20px 20px 0;
-            float:left;
-        }
-        .pingblock__item{
-            padding:20px;
-            width: calc(100% - 40px);
-            float:left;
-            transform: scale(1);
-        }
-        .status--green{
-            background-color: #28b02d;
-            color: #fff;
-        }
-        .status--red{
-            background-color: #bf2424;
-            color: #fff;
-        }
-        /*.popbox{*/
-        /*    animation-name: activity;*/
-        /*    animation-duration: 1s;*/
-        /*}*/
-        .togo__bar{
-            width: 100%;
-            height: 4px;
-            float: left;
-            position: relative;
-            margin-top: 20px;
-            background-color: rgba(255,255,255,0.3);
-        }
-        /*@keyframes activity {*/
-        /*    0% {*/
-        /*        opacity:0.5;*/
-        /*    }*/
-        /*    100% {*/
-        /*        opacity:1;*/
-        /*    }*/
-        /*}*/
-        .togo__bar span{
-             width: 100%;
-             height: 4px;
-             position: absolute;
-             right: 0;
-             top: 0;
-             background-color: #fff;
-             animation-name: togobar;
-             /*animation-iteration-count: infinite;*/
-             animation-duration: 60s;
-        }
-        @keyframes togobar {
-            0% {
-                width: 100%;
-            }
-            100% {
-                width: 0;
-            }
-        }
-    </style>
 
 </head>
 <body>
